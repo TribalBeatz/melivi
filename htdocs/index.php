efca588299b993f04a8ecd9b36856789cc332a73
Chris Pepper <p3p@p3psoft.co.uk>
2008-09-13 06:35:52 +0100

    Basic Layout (subject to major changes)

---
+++ htdocs/index.php
@@ -0,0 +1,15 @@
+<?php
+    require_once("../app/melivi.php");
+?>
+
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html>
+<head>
+    <title>Melivi</title>
+</head>
+<body>
+
+</body>
+</html>
